efa94ab074f655c35bc1605fd8dfe546665a5f8d
Jesse Kade <jessekade@gmail.com>
2012-03-03 13:23:48 -0800

    finished footer

--- wp-content/themes/white-antelope/footer.php
+++ wp-content/themes/white-antelope/footer.php
@@ -1,7 +1,31 @@
 		
 			<footer role="contentinfo">
+				<article class="span-5 colborder">
+					<h2>Invite Edie</h2>
+					<p>Sed in arcu felis, vel dictum odio. Nunc mollis nulla eget magna lobortis quis egestas risus feugiat.</p>
+					<a href="<?php bloginfo('url'); ?>/contact">Contact Edie Now</a>
+				</article>
+				<article class="span-5 colborder">
+					<h2>Upcoming Events</h2>
+					<ul class="footer-events">
+						<li><a href="#">+ Freedom to Worship</a></li>
+						<li><a href="#">+ Another Cool Event</a></li>
+						<li><a href="#">+ The Coolest Event</a></li>
+					</ul>
+					<a href="<?php bloginfo('url'); ?>/contact">See All Events</a>
+				</article>
+				<article class="span-5 colborder">
+					<h2>Last Blog Entry</h2>
+					<h3><a href="#">Using Watercolor</a></h3>
+					<p>Sed in arcu felis, vel dictum odio. Nunc mollis nulla eget magna lobortis quis egestas risus feugiat.</p>
+					<a href="<?php bloginfo('url'); ?>/contact">Continue Reading</a>
+				</article>
+				<article class="span-6 last">
+					<p>White Antelope exists to model, promote, teach and inspire art, including prophetic art, with the goal of helping others understand and harness the inherent property of healing that art carries, both through the production and appreciation of artistic works.</p>
+					<a href="#">Sign Up For Newsletter</a>
+				</article>
 				<div id="site-info">
-					&copy;<?php echo date ('Y'); ?> <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					&copy;<?php echo date ('Y'); ?> <?php bloginfo( 'name' ); ?> | <a href="<?php bloginfo('url'); ?>/terms-conditions">Terms &amp; Conditions</a> | <a href="<?php bloginfo('url'); ?>/privacy-policy">Privacy Policy</a> | <a href="#">Shipping &amp; Returns Policy</a>
 				</div><!-- #site-info -->
 			</footer>
 		</div><!-- .container -->
